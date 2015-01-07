01b9784dac4ab5a6a04a8f6eb559886a96af41bc
Admin <Admin@192.168.0.15>
2015-01-07 13:16:26 -0800

    changed the main class to include 3 students, me included

--- index.php
+++ index.php
@@ -11,8 +11,45 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
-        echo "BWAHAHAHA";
+            include('student.php');
+
+            $students = array();
+
+            $first = new Student();
+            $first->surname = "Doe";
+            $first->first_name = "John";
+            $first->add_email('home','john@doe.com');
+            $first->add_email('work','jdoe@mcdonalds.com');
+            $first->add_grade(65);
+            $first->add_grade(75);
+            $first->add_grade(55);
+            $students['j123'] = $first;
+            
+            $second = new Student();
+            $second->surname = "Einstein";
+            $second->first_name = "Albert";
+            $second->add_email('home','albert@braniacs.com');
+            $second->add_email('work1','a_einstein@bcit.ca');
+            $second->add_email('work2','albert@physics.mit.edu');
+            $second->add_grade(95);
+            $second->add_grade(80);
+            $second->add_grade(50);
+            $students['a456'] = $second;
+            
+            $third = new Student();
+            $third->surname = "Chu";
+            $third->first_name = "Jonathan";
+            $third->add_email('home','jonathan_chu1988@hotmail.com');
+            $third->add_email('school','jchu08@bcit.ca');
+            $third->add_grade(75);
+            $third->add_grade(90);
+            $third->add_grade(70);
+            $students['c789'] = $third;
+            
+            ksort($students);	// one of the many sort functions
+            
+            foreach($students as $student)
+                echo $student->toString();
         ?>
     </body>
 </html>
